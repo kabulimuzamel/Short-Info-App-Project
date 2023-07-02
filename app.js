e852be35ff4096bee487580d9e905c4679f83fcf
ZCP1 <zcp1@ZCP1s-MacBook-Pro.local>
2023-07-02 12:57:23 -0400

    Renders search at the bottom of the screen

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ $(document).ready(() => {
     const $searchButton = $('#search-button');
     const $clearButton = $('#clear-button');
     const $searchedInput = $('#searched-place');
+    const $inputGroup = $('.input-group');
     let userInput;
 
     $clearButton.hide();
@@ -20,6 +21,8 @@ $(document).ready(() => {
         e.preventDefault();
         $('.resultCard').remove();
         searchedPlace.length = 0;
+        $inputGroup.css('margin-top', '600px')
+        $searchedInput.val('')
         $clearButton.hide();
     })
 
@@ -30,23 +33,23 @@ $(document).ready(() => {
             $('#alertModal').modal('hide')
         })
     }
-
-    //Try to break your card generator  into different variables
-    //card-text should be another variable 
-
-    
+  
     function cardGenerator(className, res) {
-        const $resultCard = $(`<div class="card resultCard ${className}Card mx-3 my-3"></div>`)
+        const $resultCard = $(`<div class="card resultCard ${className}Card mx-3 my-3 position-relative"></div>`)
+            const $closeCardButton = $(`<button style='top: 10px; right: 20px' type="button" class='btn-close closeButton${className} position-absolute' aria-label="Close"></button>`)
             const $resultCardRow = $(`<div class="row ${className}Row g-0"></div>`)
                 const $resultTextColumn = $(`<div class="col-xl-8"></div>`)
                     const $resultCardHeader = $(`<div class='card-header overflow-auto'></div>`)
-                        const $cardHeaderTitle = $(`<h2 class="card-title cardTitle${className}">${res[0].name}</h2><button style='position: absolute; top: 10px; right: 10px' type="button" class='btn-close closeButton${className}' aria-label="Close"></button>`)  
+                        const $cardHeaderTitle = $(`<h2 class="card-title cardTitle${className}">${res[0].name}</h2><img class='${className}FactButton factIcon'
+                        src='https://img.icons8.com/?size=512&id=XBJfETMfZHpS&format=png'
+                        style='height:2rem; width:2rem;'/>`)  
                     const $resultCardBody = $(`<div class="card-body"></div>`)
                         const $cardBodyText = $(`<p class="card-text">
-                            ${res[0].name}, located in ${res[0].region}, has a population of approximately ${(res[0].population / 1000).toFixed(3)} million and a population growth rate of ${res[0].pop_growth}%. The country spans a surface area of ${res[0].surface_area} square kilometers with a population density of ${res[0].pop_density} people per square kilometer. ${res[0].capital} serves as its capital city. ${res[0].name}'s GDP stands at ${(res[0].gdp / 1000).toFixed(3)} billion dollars, with a GDP growth rate of ${res[0].gdp_growth}%. The country's GDP per capita is ${res[0].gdp_per_capita}, and its currency is called the ${res[0].currency.name} (${res[0].currency.code}). The sex ratio is ${res[0].sex_ratio}, and the fertility rate is ${res[0].fertility}. Life expectancy for males is ${res[0].life_expectancy_male} years, while for females, it is ${res[0].life_expectancy_female} years. The unemployment rate in ${res[0].name} is ${res[0].unemployment}%, and the urban population constitutes ${res[0].urban_population}% of the total population. Forested areas cover ${res[0].forested_area}% of the country, and ${res[0].internet_users}% of the population uses the internet.
+                            <b>${res[0].name}</b>, located in <b>${res[0].region}</b>, has a population of approximately <b>${(res[0].population / 1000).toFixed(3)} million</b> and a population growth rate of <b>${res[0].pop_growth}%</b>. The country spans a surface area of <b>${res[0].surface_area} square kilometers</b> with a population density of <b>${res[0].pop_density} people per square kilometer</b>. <b>${res[0].capital}</b> serves as its capital city. <b>${res[0].name}'s GDP</b> stands at <b>${(res[0].gdp / 1000).toFixed(3)} billion dollars</b>, with a GDP growth rate of <b>${res[0].gdp_growth}%</b>. The country's GDP per capita is <b>${res[0].gdp_per_capita}</b>, and its currency is called the <b>${res[0].currency.name} (${res[0].currency.code})</b>. The sex ratio is <b>${res[0].sex_ratio}</b>, and the fertility rate is <b>${res[0].fertility}</b>. Life expectancy for males is <b>${res[0].life_expectancy_male} years</b>, while for females, it is <b>${res[0].life_expectancy_female} years</b>. The unemployment rate in <b>${res[0].name}</b> is <b>${res[0].unemployment}%</b>, and the urban population constitutes <b>${res[0].urban_population}%</b> of the total population. Forested areas cover <b>${res[0].forested_area}%</b> of the country, and <b>${res[0].internet_users}% of the population</b> uses the internet.
                         </p>`)
 
         $resultCard.append($resultCardRow);
+        $resultCard.append($closeCardButton);
         $resultCardRow.append($resultTextColumn);
             $resultTextColumn.append($resultCardHeader);
                 $resultCardHeader.append($cardHeaderTitle);
@@ -67,12 +70,29 @@ $(document).ready(() => {
             .then((res) => res.json())
             .then((event) => {
                 $(`.cardTitle${className}`).after(`
-                    <span class='card-subtitle'>
+                    <span class='${className}-card-subtitle cardSub'>
                         Did you know that on ${event[0].day}/${event[0].month}/${event[0].year} ${event[0].event}
                     </span>
-                    `)
+                `);
+                hideFact(className)
             })
         
+    }
+
+    function factButton(className) {
+        $(`.${className}FactButton`).on('click', (e) => {
+			e.preventDefault()
+            if($(`.${className}-card-subtitle`).is(`:hidden`)) {
+                $(`.${className}-card-subtitle`).show()
+            } else {
+                $(`.${className}-card-subtitle`).hide()
+            }
+		})
+    }
+
+
+    function hideFact(className) {
+        $(`.${className}-card-subtitle`).hide()
     }
 
     function imgGenerator(imgUrl, className) {
@@ -120,37 +140,20 @@ $(document).ready(() => {
                     if (searchedPlace.includes(className)) {
                         showAlert('You already have one search result for this country!')
                     } else {
+                        $(`.input-group`).css('margin-top', '50px')
                         searchedPlace.push(className);
                         $clearButton.show();
                         $resultContainer.prepend(cardGenerator(className, res))
                         historyEventGenerator(historyEventURL, className);
                         imgGenerator(imgUrl, className);
                         cardCloseButtonGenerator(className, searchedPlace);
+                        factButton(className);
                     }
 
                 })
                 .catch(() => {
                     showAlert("We couldn't find the name of the country you were looking for, please try in a different way!");
                 })          
-
         } 
     }
 })
-
-
-
-        // return `<div class="card resultCard ${className}Card mx-3 my-3">
-        //             <div class="row ${className}Row g-0">
-        //                 <div class="col-xl-8">
-        //                     <div class='card-header overflow-auto'>
-        //                         <h2 class="card-title cardTitle${className}">${res[0].name}</h2>
-        //                         <button style='position: absolute; top: 10px; right: 10px' type="button" class='btn-close closeButton${className}' aria-label="Close"></button>
-        //                     </div>
-        //                     <div class="card-body">
-        //                         <p class="card-text">
-        //                             ${res[0].name}, located in ${res[0].region}, has a population of approximately ${(res[0].population / 1000).toFixed(3)} million and a population growth rate of ${res[0].pop_growth}%. The country spans a surface area of ${res[0].surface_area} square kilometers with a population density of ${res[0].pop_density} people per square kilometer. ${res[0].capital} serves as its capital city. ${res[0].name}'s GDP stands at ${(res[0].gdp / 1000).toFixed(3)} billion dollars, with a GDP growth rate of ${res[0].gdp_growth}%. The country's GDP per capita is ${res[0].gdp_per_capita}, and its currency is called the ${res[0].currency.name} (${res[0].currency.code}). The sex ratio is ${res[0].sex_ratio}, and the fertility rate is ${res[0].fertility}. Life expectancy for males is ${res[0].life_expectancy_male} years, while for females, it is ${res[0].life_expectancy_female} years. The unemployment rate in ${res[0].name} is ${res[0].unemployment}%, and the urban population constitutes ${res[0].urban_population}% of the total population. Forested areas cover ${res[0].forested_area}% of the country, and ${res[0].internet_users}% of the population uses the internet.
-        //                         </p>                        
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>`
